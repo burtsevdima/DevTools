c8ad6a33bcdbc91fa45f1afb1d09412563444dfb
Shoshta73 <128172135+Shoshta73@users.noreply.github.com>
2023-10-23 11:55:12 +0200

    implent simple commandline argument parser

--- Cher/main.go
+++ Cher/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"runtime"
 
 	consts "github.com/Shohsta73/DevTools/Cher/constants"
+	"github.com/Shohsta73/DevTools/Cher/parser"
 )
 
 func helpMessage() {
@@ -13,6 +16,53 @@ func helpMessage() {
 		"init | i   initialize Cher\n" +
 		"help | h   this help message",
 	)
+}
+
+func initCher() (string, error) {
+	configDir, err := getConfigDir()
+	if configDir == "" {
+		return "", errors.New("config directory not found")
+	}
+	if err != nil {
+		return "", err
+	}
+
+	_, err = os.Stat(configDir)
+
+	if os.IsNotExist(err) {
+		// Directory doesn't exist, create it
+		err = os.Mkdir(configDir, 0755) // 0755 is the directory permission, you can adjust it as needed
+		if err != nil {
+			return "", err
+		}
+
+		fmt.Printf(
+			"We do not provide default .editorconfig files.\n"+
+				"You will have to create your own in %s.\n"+
+				"We recomande ussing directories <lang> for specif languages.\n", configDir,
+		)
+	} else if err != nil {
+		return "", err // Some other error occurred
+	}
+
+	return configDir, nil
+}
+
+func getConfigDir() (string, error) {
+	var configDir string
+	userDir, err := os.UserConfigDir()
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	if runtime.GOOS == "windows" {
+		configDir = userDir + "\\Cher"
+	} else {
+		configDir = userDir + "/Cher"
+	}
+
+	return configDir, nil
 }
 
 func main() {
@@ -32,30 +82,85 @@ func main() {
 		fmt.Println()
 	}
 
-	if len(os.Args) < 2 {
+	Parser := parser.NewParser()
+
+	configDir, err := getConfigDir()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(configDir)
+
+	parsedCommands, err := Parser.Parse(os.Args[1:])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if parsedCommands.ParsedCommands["help"] {
 		helpMessage()
 		return
 	}
 
-	switch os.Args[1] {
-	case "help":
-		fallthrough
-	case "h":
-		helpMessage()
-		return
-	case "init":
-		fallthrough
-	case "i":
-		switch os.Args[2] {
-		case "--help":
-			fallthrough
-		case "-h":
-			fmt.Println(
-				"usage: Cher init | i <flags> [langugaes]\n" +
-					"flags:\n" +
-					"--help | -h   this help message",
-			)
+	if parsedCommands.ParsedCommands["init"] {
+		_, err := initCher()
+		if err != nil {
+			fmt.Println(err)
 			return
 		}
+		return
+	}
+
+	if parsedCommands.ParsedCommands["add"] {
+		var DirSep string
+		if runtime.GOOS == "windows" {
+			DirSep = "\\"
+		} else {
+			DirSep = "/"
+		}
+		for i := 2; i < len(os.Args); i++ {
+			if os.Args[i] == "help" {
+				helpMessage()
+				return
+			}
+
+			langDir := configDir + DirSep + os.Args[i]
+			_, err = os.Stat(langDir)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+		return
+	}
+
+	if parsedCommands.ParsedCommands["new"] {
+		var DirSep string
+		if runtime.GOOS == "windows" {
+			DirSep = "\\"
+		} else {
+			DirSep = "/"
+		}
+		return
+	}
+
+	if parsedCommands.ParsedCommands["remove"] {
+		var DirSep string
+		if runtime.GOOS == "windows" {
+			DirSep = "\\"
+		} else {
+			DirSep = "/"
+		}
+		return
+	}
+
+	if parsedCommands.ParsedCommands["edit"] {
+		var DirSep string
+		if runtime.GOOS == "windows" {
+			DirSep = "\\"
+		} else {
+			DirSep = "/"
+		}
+		return
 	}
 }
